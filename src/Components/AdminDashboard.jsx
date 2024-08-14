d62a60b173189661496c056fd35d4965ffa82c55
RamSundar9894 <ramsundar9894@gmail.com>
2024-08-14 09:44:51 +0530

    changes made to the admin AdminDashboard file

--- src/Components/AdminDashboard.jsx
+++ src/Components/AdminDashboard.jsx
@@ -4,50 +4,6 @@ import AdminSidebar from './AdminSidebar'; // Import the sidebar
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const AdminDashboard = () => {
-  const [products, setProducts] = useState(0);
-  const [categories, setCategories] = useState(0);
-  const [users, setUsers] = useState(0);
-
-  useEffect(() => {
-    // Simulate the ticker effect
-    let productsCount = 0;
-    let categoriesCount = 0;
-    let usersCount = 0;
-
-    const productsInterval = setInterval(() => {
-      if (productsCount < 100) {
-        productsCount += 1;
-        setProducts(productsCount);
-      } else {
-        clearInterval(productsInterval);
-      }
-    }, 10);
-
-    const categoriesInterval = setInterval(() => {
-      if (categoriesCount < 5) {
-        categoriesCount += 1;
-        setCategories(categoriesCount);
-      } else {
-        clearInterval(categoriesInterval);
-      }
-    }, 30);
-
-    const usersInterval = setInterval(() => {
-      if (usersCount < 150) {
-        usersCount += 1;
-        setUsers(usersCount);
-      } else {
-        clearInterval(usersInterval);
-      }
-    }, 20);
-
-    return () => {
-      clearInterval(productsInterval);
-      clearInterval(categoriesInterval);
-      clearInterval(usersInterval);
-    };
-  }, []);
-
   return (
     <div className='admin-body'>
       <div className="admin-dashboard-container">
@@ -56,21 +12,6 @@ const AdminDashboard = () => {
           <h1>Admin Dashboard</h1>
           <br></br>
           <br></br>
-          {/* <br></br> */}
-          <div className="admin-stats-container">
-            <div className="admin-stats-card">
-              <h3>{users}+ Users</h3>
-              <p>Total number of registered users.</p>
-            </div>
-            <div className="admin-stats-card">
-              <h3>{products}+ Products</h3>
-              <p>Total number of available products.</p>
-            </div>
-            <div className="admin-stats-card">
-              <h3>{categories}+ Categories</h3>
-              <p>Total number of product categories.</p>
-            </div>
-          </div>
           <div className="admin-recent-activities">
             <h3>Recent Activities</h3>
             <div className="admin-activity-card">
