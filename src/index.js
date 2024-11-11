094378db98a1ddbc15cc847c0bf347ca23c0a497
RamSundar9894 <ramsundar9894@gmail.com>
2024-11-11 07:38:42 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' for React 18
 import App from './App';
 import { AuthProvider } from './Components/AuthContext';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root element
+root.render(
     <AuthProvider>
         <App />
-    </AuthProvider>,
-    document.getElementById('root')
+    </AuthProvider>
 );
